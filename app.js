f113dd9be8170041915098f992c4d4e30377e6f2
AlexisLebonDev <alexislebon.dev@gmail.com>
2023-10-04 16:12:10 +0400

    ajout des modifications serveur

--- app.js
+++ app.js
@@ -11,3 +11,25 @@ import { getAlltask, getTaskFromIdUser, getTaskFromNameUser } from "./controller
 
 // getTaskFromIdUser(2);
 
+
+import express from 'express'
+
+const app = express()
+
+//Mon serveur qui s'appel APP, quand tu reçois une requête GET sur le chemin "/" et bien renvoie dans la réponse un message ex:"Hello WOrld"
+app.get('/json', function (req, res) {
+  //Ici c'est la partie controlleur qui me permet de faire de calcul, ou aller chercher des données avant de l'envoyer au client
+  res.json({
+    "prenom": "Jean",
+    "age" : 28
+  })
+})
+
+// Mon serveur qui s'appel APP, quand tu reçois une requête GET sur le chemin "/" et bien renvoie dans la réponse un message ex:"Hello WOrld"
+app.get('/', function (req, res) {
+  res.send('Coucou')
+})
+
+app.listen(3000,()=>{
+  console.log("serveur démarré sur local host")
+})
